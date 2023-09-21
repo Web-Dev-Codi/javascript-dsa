4004118251e9150648e6be9ef9908f6626c4783d
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-09-21 13:21:49 +0200

    coding bubble sort

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,12 +1,11 @@
 function findMaxNumber(arr) {
 
-for (let index = 0; index < arr.length; index++) {
-    const element = arr[index];
-    console.log(element.sorted());
-}
-  
-    
-    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i]) {
+            console.log([i].sort());
+        }
+
+    }
 }
 
 module.exports = findMaxNumber;
