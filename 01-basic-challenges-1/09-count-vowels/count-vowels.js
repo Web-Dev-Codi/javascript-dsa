fb41e135f3d6d02db73411e7e59f6df5b101fcda
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-09-22 16:20:49 +0200

    Nailed Vowel Challenge

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,14 @@
-function countVowels() {}
+function countVowels(str) {
+    let vowels = /^[aAeEiIoOuU]+$/;
+
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].match(vowels)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 module.exports = countVowels;
