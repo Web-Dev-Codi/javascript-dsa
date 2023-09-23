ab199b0f0c8a048ba6f08113bc7cd3ba57f86265
Web-Dev-Codi <cordiscobrian@gmail.com>
2023-09-23 13:40:48 +0200

    Crushed array-intersection

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,20 @@
-function removeDuplicates() {}
+function swap(array, index1, index2) {
+   let temp = array[index1];
+   array[index1] = array[index2];
+   array[index2] = temp;
+}
+
+function removeDuplicates(arr) {
+   let newArray = [];
+
+   for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j <= i; j++) {
+         if (arr[i] < arr[j]) {
+            swap(arr, i, j)
+         }
+      }
+   }
+   return arr;
+}
 
 module.exports = removeDuplicates;
--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,21 @@
-function arrayIntersection() {}
+function swap(array, index1, index2) {
+    let temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+}
+
+
+function arrayIntersection(arrOne, arrTwo) {
+    var newArray = new Array()
+
+    for (let i = 0; i < arrOne.length; i++) {
+        for (let j = 0; j <= arrTwo.length; j++) {
+            if (arrOne[i] === arrTwo[j]) {
+                newArray.push(arrTwo[j])
+            }
+        }
+    }
+    return newArray;
+}
 
 module.exports = arrayIntersection;
